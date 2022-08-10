ef223f35e9e3e8710a01fe7d8397a887007cff17
Grizzly <makintoch2104@gmail.com>
2022-08-10 22:47:57 +0300

    fisrt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,21 @@ import { HeaderComponent } from './header/header.component';
 
 // PrimeNG
 import { MenubarModule } from 'primeng/menubar';
-import { MenuItem } from 'primeng/api';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, MenubarModule],
+  imports: [
+    BrowserModule,
+    MenubarModule,
+    AutoCompleteModule,
+    FormsModule,
+    ButtonModule,
+    InputTextModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
