94b08160f66cac31dea1941da7ddeb0b4ef9fff0
grizzly21 <34778670+grizzly21@users.noreply.github.com>
2022-09-09 19:53:10 +0300

    created pop up for adding tasks and create table for tasks

--- src/app/dashboard/components/tasks/tasks.component.ts
+++ src/app/dashboard/components/tasks/tasks.component.ts
@@ -3,13 +3,48 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.scss']
+  styleUrls: ['./tasks.component.scss'],
 })
 export class TasksComponent implements OnInit {
+  filterToggle: boolean = false;
+  popUpToggle: boolean = false;
 
-  constructor() { }
+  tasksArr = [
+    {
+      task: 'Your task nomer 1',
+      date: 'today',
+      preformer: 'user 1',
+      author: 'Admin',
+    },
+    {
+      task: 'Your task nomer 1',
+      date: 'today',
+      preformer: 'user 1',
+      author: 'Admin',
+    },
+    {
+      task: 'Your task nomer 1',
+      date: 'today',
+      preformer: 'user 1',
+      author: 'Admin',
+    },
+    {
+      task: 'Your task nomer 1',
+      date: 'today',
+      preformer: 'user 1',
+      author: 'Admin',
+    },
+  ];
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onFilterToggle() {
+    this.filterToggle = !this.filterToggle;
   }
 
+  onPopUpToggle(){
+    this.popUpToggle = !this.popUpToggle;
+  }
 }
